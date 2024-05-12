c7ebfab0127b31d227adbf1d8612ffb935370891
BREW <IRACK000@gmail.com>
2024-05-12 14:28:42 +0900

    Chore: Correct setup 2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author="daiv",
     author_email="cnudaiv@gmail.com",
     url="https://github.com/dAiv-CNU/torchdaiv",
-    packages=list(set(['/'.join(path.removesuffix(".py").replace("\\", "/").split('/')[:-1]) for path in glob("**/*.py", recursive=True)])),
+    packages=list(set(['/'.join(path.removesuffix(".py").replace("\\", "/").split('/')[:-1]) for path in glob("src/main/**/*.py", recursive=True)])),
     install_requires=[
         "torch",
         "torchvision",
