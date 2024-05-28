0e5397dbc4542015c8cca42ce9e977b9e5af93d9
BREW <IRACK000@gmail.com>
2024-05-28 18:40:46 +0900

    Fix: Fix Error

--- src/torchdaiv/datasets/anki_kor_eng.py
+++ src/torchdaiv/datasets/anki_kor_eng.py
@@ -32,8 +32,9 @@ class AnkiKorEngDataset(Dataset):
             self._download(download_root=root)
             self._raw_data, self.info = self._load_data(data_dir=join(root, self.base_dir))
 
-            #self.raw_eng, self.raw_kor = zip(*self._raw_data)
-            for eng, kor in zip(*self._raw_data):
+            raw_eng, raw_kor = zip(*self._raw_data)
+
+            for eng, kor in zip(raw_eng, raw_kor):
                 if "자살" in kor or "죽" in kor:
                     continue
                 self.raw_eng.append(eng)
