583c5b40de38137cdf620620848b929abb20af0d
BREW <IRACK000@gmail.com>
2024-05-28 18:34:48 +0900

    Fix: Fix Typo

--- src/torchdaiv/datasets/anki_kor_eng.py
+++ src/torchdaiv/datasets/anki_kor_eng.py
@@ -33,7 +33,7 @@ class AnkiKorEngDataset(Dataset):
             self._raw_data, self.info = self._load_data(data_dir=join(root, self.base_dir))
 
             #self.raw_eng, self.raw_kor = zip(*self._raw_data)
-            for eng, kor in zip(self._raw_data):
+            for eng, kor in zip(*self._raw_data):
                 if "자살" in kor or "죽" in kor:
                     continue
                 self.raw_eng.append(eng)
